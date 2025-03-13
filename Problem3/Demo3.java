f125ae6f04f7e090086385c28549d80b01b6b64f
Noah Devine <Noah.Devine@keyin.com>
2025-03-13 15:55:01 -0230

    Finished coding the demo class and equilateral class
    - Finished coding both the testing demo class and the final EquilateralTriangle class.

--- Problem3/Demo3.java
+++ Problem3/Demo3.java
@@ -1,5 +1,16 @@
+// Problem #3
 public class Demo3 {
     public static void main(String[] args) {
-        
+        Shape[] shapes = new Shape[4];
+        shapes[0] = new Circle("Circle", 5);
+        shapes[1] = new Triangle("Triangle", 3, 4, 5);
+        shapes[2] = new Ellipse("Ellipse", 6, 4);
+        shapes[3] = new EquilateralTriangle("Equilateral Triangle", 6);
+
+        System.out.println("Testing for the Different Shapes:\n");
+        for (Shape shape : shapes) {
+            System.out.println(shape);
+            System.out.println(); // Added a blank line for readability
+        }
     }
 }
--- Problem3/EquilateralTriangle.java
+++ Problem3/EquilateralTriangle.java
@@ -1,3 +1,16 @@
-public class EquilateralTriangle {
-    
+// Problem #3
+public class EquilateralTriangle extends Triangle {
+    // Instance variable for side length (all sides are equal)
+    private double side;
+
+    // Constructor
+    public EquilateralTriangle(String name, double side) {
+        super(name, side, side, side);  
+        this.side = side;
+    }
+
+    // Getter for side
+    public double getSide() {
+        return side;
+    }
 }
